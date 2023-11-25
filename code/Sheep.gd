5fafd2392ab476b81615bfc8e80b1ecc1155c5b7
Tiger Jove <tiger@jupiter.blue>
2023-11-25 16:11:11 +0100

    simple evastion

---
+++ code/Sheep.gd
@@ -0,0 +1,35 @@
+extends RigidBody3D
+
+var dog : Node3D
+var mirrored : bool
+
+var move_direction : Vector3 = Vector3.ZERO
+@export var move_speed : float = 1
+
+func _physics_process(delta):
+	if dog:
+		chase_or_flee()
+	else:
+		move_direction = Vector3.ZERO
+	add_constant_central_force(move_direction * move_speed)	
+	
+	
+			
+func chase_or_flee():
+	var direction : Vector3 = (self.global_position - dog.global_position)
+	if mirrored:
+		direction *= -1
+
+	move_direction = direction.normalized()
+	
+func _on_area_3d_body_entered(body):
+	if body.name == "Dog Controller":
+		dog = body
+
+
+func _on_area_3d_area_exited(area):
+	dog = null
+
+
+func _on_area_3d_body_exited(body):
+	pass # Replace with function body.
