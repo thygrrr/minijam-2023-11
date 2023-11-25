8c8ba5a3cec48bc58df257fc558b18d6b044ae70
Saskia Peschutter <49360233+SaskiaPeschutter@users.noreply.github.com>
2023-11-25 17:02:36 +0100

    added goal with win condition

---
+++ code/Goal.gd
@@ -0,0 +1,24 @@
+extends Node3D
+@export var sheep_count_goal : int = 1
+var sheep_count : int
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	sheep_count = 0
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_3d_area_entered(area):
+	print(sheep_count)
+	if area.name == "Sheep Area":
+		sheep_count += 1 
+		if sheep_count >= sheep_count_goal:
+			print("win")
+			
+
+func _on_area_3d_area_exited(area):
+	sheep_count -= 1 
