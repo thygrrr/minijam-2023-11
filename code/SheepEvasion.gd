9c43fab8e22abcdd069e739b8fb2f0babb1bddfc
Tiger Jove <tiger@jupiter.blue>
2023-11-25 15:32:46 +0100

    Sheep evasion test

--- code/SheepEvasion.gd
+++ code/SheepEvasion.gd
@@ -1,1 +1,16 @@
 extends Node
+
+var dog : Node3D
+
+func _on_rigid_body_3d_body_entered(body):
+	dog = body
+
+
+func _on_rigid_body_3d_body_exited(body):
+	dog = null
+	
+func _process(delta):
+	#Move away from dog
+	if dog:
+		print("want to move away.")
+	
