55c4063a4da7d92994003ebd74662cbf1c837e2c
Tiger Jove <tiger@jupiter.blue>
2023-11-25 15:52:05 +0100

    simple evasion response

--- code/SheepEvasion.gd
+++ code/SheepEvasion.gd
@@ -1,16 +1,17 @@
 extends Node
 
 var dog : Node3D
-
-func _on_rigid_body_3d_body_entered(body):
-	dog = body
-
-
-func _on_rigid_body_3d_body_exited(body):
-	dog = null
 	
 func _process(delta):
 	#Move away from dog
 	if dog:
 		print("want to move away.")
 	
+
+func _on_area_3d_body_entered(body):
+	if body.name == "Dog Controller":
+		dog = body
+
+
+func _on_area_3d_area_exited(area):
+	dog = null
